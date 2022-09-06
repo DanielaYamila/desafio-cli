72fd0861d0f0fa28167052bf3b7722e17081e573
Daniela Gomez <danielagomezyamila@gmail.com>
2022-09-05 23:03:36 -0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <nav>
+      <header>
+        <a id="titulo">VIBES ZONE</a>
       </header>
-    </div>
+      <ul>
+        <li>
+          <a>INICIO</a>
+        </li>
+        <li>
+          <a>SAHUMERIOS</a>
+        </li>
+        <li>
+          <a>FRAGANCIAS</a>
+        </li>
+        <li>
+          <a>VELAS</a>
+        </li>
+        <li>
+          <a>PIEDRAS</a>
+        </li>
+        <li>
+          <a>CONTACTO</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
